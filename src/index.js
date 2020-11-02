fbf3b6505ef780bc79d1445eeefcec874e0a2a53
audisiofilho <obuzua46@gmail.com>
2020-11-02 11:57:59 -0300

    tipos de parametros

--- src/index.js
+++ src/index.js
@@ -2,7 +2,17 @@ const { response, request } = require("express")
 const express = require("express") //importamos
 const app = express() //atribuimos as funçõs a variavel
 
+app.use(express.json())
+
+//query params (listar, filtrar minhas informações)
+//route params (identificar recursos -> atulaizar e deletar)
+//request body (criar ou editiar arquivos -> JSON)
+
 app.get("/projeto", (request, response) => {
+    const {title, dev} = request.query
+    console.log(title)
+    console.log(dev)
+
     return response.json([
          "Projeto 1",
          "Projeto 2"
@@ -10,6 +20,10 @@ app.get("/projeto", (request, response) => {
 })
 
 app.post('/projeto', (request, response) => {
+    const body = request.body
+    console.log(body)
+
+
     return response.json([
         "Projeto 1",
         "Projeto 2",
@@ -17,7 +31,10 @@ app.post('/projeto', (request, response) => {
     ])
 })
 
-app.put('/projeto:id', (request, response) =>{
+app.put('/projeto/:id', (request, response) =>{
+    const {id} = request.params
+    console.log(id)
+
     return response.json([
         "Projeto 4",
         "Projeto 2",
@@ -25,7 +42,7 @@ app.put('/projeto:id', (request, response) =>{
     ])
 })
 
-app.delete('/projeto:id', (request, response) =>{
+app.delete('/projeto/:id', (request, response) =>{
     return response.json([
         "Projeto 2",
         "Projeto 3"
