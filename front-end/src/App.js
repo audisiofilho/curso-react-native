89d3c30093b6b01f658a666b27ec4168b57abd3b
audisiofilho <obuzua46@gmail.com>
2020-11-09 14:59:18 -0300

    Componentização

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,9 +1,14 @@
-
+import React from 'react'
+import Header from './components/Header'
 
 
 function App() {
   return (
-    <h1>Ola Mundo</h1>
+    <>
+     <Header/>
+     <Header/>
+     <Header/>
+    </>
   );
 }
 
